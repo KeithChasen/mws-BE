efd2bffce61b4b335ca340dc79d1c21b83fff4a2
keithchasen <keithchasen89@gmail.com>
2021-05-19 15:20:22 +0300

    optimized auth validation: use validate login in validate register

--- utils/validations/auth.js
+++ utils/validations/auth.js
@@ -1,19 +1,7 @@
 const emailRegEp = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
 
 const validateRegister = (email, password, confirmPassword) => {
-  const errors = {};
-
-  if (email.trim() === '') {
-    errors.email = 'Email is required';
-  }
-
-  if (!email.match(emailRegEp)) {
-    errors.email = 'Email should be a valid email address';
-  }
-
-  if (password.trim() === '') {
-    errors.password = 'Password is required';
-  }
+  const errors = validateLogin(email, password);
 
   if (password.length < 8) {
     errors.password = 'Password should be at least 8 characters long';
